492f0b80ffc5cd76665c81675d4b379e4183cd47
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-07-22 13:22:08 -0400

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -42,8 +42,8 @@ new Vue({
 
         function DrawRect(e, t, r) {
           if (e / h < p.abs(p.noise((p.mouseX / 5 - t) / h + 1, (p.mouseY / 5 - r) / h + p.frameCount / 5000, p.mag(t, r) / w) - 0.5)) {
-            p.fill((thisHue + 5 * e) % 360, 70 - 5 * e, 70 - e, 1 - 0.028 * e);
-            p.rect(t, r, 1.3 * e, 1.2 * e);
+            p.fill((thisHue + 5 * e) % 360, 70 - 5 * e, 70 - e, 1 - 0.05 * e);
+            p.rect(t, r, 2.5 * e, 2.5 * e);
           } else if (e /= 2, e > 2.5) {
             DrawRect(e, t - e, r - e);
             DrawRect(e, t + e, r - e);
